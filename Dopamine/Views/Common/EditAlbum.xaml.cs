5d840b5e8a0d391c2c239a00ea74fa8e837e6d4b
yyy-win11 <1830979240@qq.com>
2026-03-05 17:44:30 +0800

    feat: enable quick copy of album information in edit dialog

--- Dopamine/Views/Common/EditAlbum.xaml.cs
+++ Dopamine/Views/Common/EditAlbum.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using Dopamine.ViewModels.Common;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Dopamine.Views.Common
 {
@@ -7,6 +10,36 @@ namespace Dopamine.Views.Common
         public EditAlbum()
         {
             InitializeComponent();
+        }
+
+        private void CopyAlbumTitleToClipboard(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is EditAlbumViewModel vm && !string.IsNullOrEmpty(vm.AlbumViewModel.AlbumTitle))
+            {
+                try
+                {
+                    Clipboard.SetText(vm.AlbumViewModel.AlbumTitle);
+                }
+                catch
+                {
+                    // 可选：失败处理或提示用户
+                }
+            }
+        }
+
+        private void CopyAlbumArtistToClipboard(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is EditAlbumViewModel vm && !string.IsNullOrEmpty(vm.AlbumViewModel.AlbumArtist))
+            {
+                try
+                {
+                    Clipboard.SetText(vm.AlbumViewModel.AlbumArtist);
+                }
+                catch
+                {
+                    // 可选：失败处理或提示用户
+                }
+            }
         }
     }
 }
